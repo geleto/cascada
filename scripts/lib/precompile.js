acde886a5f4cafe68bbbd3f7efebe92581445925
Angel Popov <geleto@gmail.com>
2024-12-15 09:50:14 +0000

    Improve precompile script - file encoding, error handling / console.error feedback

--- scripts/lib/precompile.js
+++ scripts/lib/precompile.js
@@ -2,20 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const precompile = require('../../nunjucks/src/precompile').precompile;
 
-var testDir = path.join(__dirname, '../../tests');
+const testDir = path.join(__dirname, '../../tests');
+const templateDir = path.join(testDir, 'templates');
+const outputFile = path.join(testDir, 'browser/precompiled-templates.js');
 
-function precompileTestTemplates() {
-  return new Promise((resolve, reject) => {
-    try {
-      const output = precompile(path.join(testDir, 'templates'), {
-        include: [/\.(njk|html)$/],
-      });
-      fs.writeFileSync(path.join(testDir, 'browser/precompiled-templates.js'), output);
-      resolve();
-    } catch (err) {
-      reject(err);
-    }
-  });
+async function precompileTestTemplates() {
+  try {
+    // Generate precompiled content
+    const output = precompile(templateDir, {
+      include: [/\.(njk|html)$/],
+    });
+
+    // Write file with explicit encoding
+    fs.writeFileSync(outputFile, output, { encoding: 'utf8', flag: 'w' });
+
+    return true;
+  } catch (err) {
+    console.error('Error precompiling templates:', err);
+    throw err;
+  }
 }
 
 module.exports = precompileTestTemplates;
