b5c7da11e21453c4197a7f0a1bcaf7bc115a98e4
Angel Popov <geleto@gmail.com>
2025-01-04 12:06:37 +0000

    Some todos

--- nunjucks/src/compiler.js
+++ nunjucks/src/compiler.js
@@ -688,14 +688,21 @@ class Compiler extends Obj {
     }
   }
 
+  //todo - do not resolve, instead resolve it at the point of use: output or argument to functions, filters. Add tests
+  // or better - return a promise
+  //maybe resolve the unused/not-last elements?
   compileGroup(node, frame) {
     this._compileAggregate(node, frame, '(', ')', true, true);
   }
 
+  //todo - do not resolve, instead resolve it at the point of use: output or argument to functions, filters. Add tests
+  //or better - return a promise
   compileArray(node, frame) {
     this._compileAggregate(node, frame, '[', ']', true, true);
   }
 
+  //todo - Add other usage tests - function argument, filter argument, output
+  //maybe return a promise if there are async properties
   compileDict(node, frame) {
     //do not resolve dictionary values, this is handled by memberLookupAsync
     this._compileAggregate(node, frame, '{', '}', false, true);
