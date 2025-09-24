d84a6b9621911cf17796877495e0696b90b4b277
Angel Popov <geleto@gmail.com>
2025-09-24 01:03:36 +0100

    Add tests for async script/template error path and the context binding that makes this work

---
+++ tests/pasync-context-binding.js
@@ -0,0 +1,138 @@
+(function () {
+  'use strict';
+
+  var expect;
+  var AsyncEnvironment;
+  var StringLoader;
+  var path;
+  var util;
+
+  if (typeof require !== 'undefined') {
+    expect = require('expect.js');
+    path = require('path');
+    AsyncEnvironment = require('../src/environment').AsyncEnvironment;
+    StringLoader = require('./util').StringLoader;
+    util = require('./util');
+  } else {
+    expect = window.expect;
+    AsyncEnvironment = nunjucks.AsyncEnvironment;
+    StringLoader = window.util.StringLoader;
+    // eslint-disable-next-line no-unused-vars
+    path = {
+      join: function () {
+        return Array.prototype.join.call(arguments, '/');
+      }
+    };
+    // eslint-disable-next-line no-unused-vars
+    util = window.util;
+  }
+
+  describe.only('Async mode - context binding', function () {
+    it('should maintain correct context for global functions', async () => {
+      var env = new AsyncEnvironment(new StringLoader());
+      env.addGlobal('myGlobal', function () {
+        return this.lookup('foo');
+      });
+      const result = await env.renderTemplateString('{{ myGlobal() }}', { foo: 'bar' });
+      expect(result).to.equal('bar');
+    });
+
+    it('should maintain correct context for data methods', async () => {
+      var env = new AsyncEnvironment(new StringLoader());
+      const context = {
+        foo: 'bar',
+        myFunc: function () {
+          return this.foo;
+        }
+      };
+      const result = await env.renderTemplateString('{{ myFunc() }}', context);
+      expect(result).to.equal('bar');
+    });
+
+    it('should maintain correct context for nested data methods', async () => {
+      var env = new AsyncEnvironment(new StringLoader());
+      const context = {
+        data: {
+          foo: 'bar',
+          myFunc: function () {
+            return this.foo;
+          }
+        }
+      };
+      const result = await env.renderTemplateString('{{ data.myFunc() }}', context);
+      expect(result).to.equal('bar');
+    });
+
+    it('should preserve this context for imported regular functions', async () => {
+      var loader = new StringLoader();
+      var env = new AsyncEnvironment(loader);
+      loader.addTemplate('context-export-func-lib.njk', '{% set myExport = myFunc %}');
+      loader.addTemplate('context-export-func-main.njk', '{% import "context-export-func-lib.njk" as lib %}{{ lib.myExport() }}');
+
+      const myObj = {
+        foo: 'bar',
+        getFoo: function () {
+          return this.foo;
+        }
+      };
+      const context = {
+        myFunc: myObj.getFoo.bind(myObj)
+      };
+      const result = await env.renderTemplate('context-export-func-main.njk', context);
+      expect(result).to.equal('bar');
+    });
+
+    describe('Async Loop Context', function () {
+      let env;
+      beforeEach(() => {
+        env = new AsyncEnvironment(new StringLoader());
+      });
+
+      it('should have correct `this` context in async for loop', async () => {
+        const context = {
+          items: [1, 2],
+          myMethod: function () {
+            return this.items.length;
+          }
+        };
+        const result = await env.renderTemplateString(
+          '{% for item in items %}{{ this.myMethod() }}{% endfor %}',
+          context
+        );
+        expect(result).to.equal('22');
+      });
+
+      it('should have correct `this` context in async while loop', async () => {
+        const context = {
+          i: 0,
+          async shouldContinue() {
+            this.i++;
+            return this.i <= 2;
+          },
+          myMethod: function () {
+            return 'method called';
+          }
+        };
+        const result = await env.renderTemplateString(
+          '{% while this.shouldContinue() %}{{ this.myMethod() }};{% endwhile %}',
+          context
+        );
+        expect(result).to.equal('method called;method called;');
+      });
+
+      it('should have correct `this` context in else block of async for loop', async () => {
+        const context = {
+          items: [],
+          myMethod: function () {
+            return 'method called';
+          }
+        };
+        const result = await env.renderTemplateString(
+          '{% for item in items %}{{ item }}{% else %}{{ this.myMethod() }}{% endfor %}',
+          context
+        );
+        expect(result).to.equal('method called');
+      });
+    });
+  });
+})();
---
+++ tests/pasync-error-reporting.js
@@ -0,0 +1,140 @@
+(function () {
+  'use strict';
+
+  var expect;
+  var AsyncEnvironment;
+  var StringLoader;
+  var path;
+
+  if (typeof require !== 'undefined') {
+    expect = require('expect.js');
+    path = require('path');
+    AsyncEnvironment = require('../src/environment').AsyncEnvironment;
+    StringLoader = require('./util').StringLoader;
+  } else {
+    expect = window.expect;
+    AsyncEnvironment = nunjucks.AsyncEnvironment;
+    StringLoader = window.util.StringLoader;
+    // eslint-disable-next-line no-unused-vars
+    path = {
+      join: function () {
+        return Array.prototype.join.call(arguments, '/');
+      }
+    };
+  }
+
+  describe.only('Async mode - error path reporting', function () {
+    var loader;
+    beforeEach(function () {
+      loader = new StringLoader();
+    });
+
+    it('should report correct path for basic error', async () => {
+      var templateName = 'error-basic.njk';
+      loader.addTemplate(templateName, '{{ nonExistentFunction() }}');
+      let env = new AsyncEnvironment(loader);
+      try {
+        await env.renderTemplate(templateName, {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain(`(${templateName})`);
+      }
+    });
+
+    it('should report correct path for included template', async () => {
+      var mainTemplateName = 'error-include-main.njk';
+      var childTemplateName = 'error-include-child.njk';
+      loader.addTemplate(mainTemplateName, '{% include "' + childTemplateName + '" %}');
+      loader.addTemplate(childTemplateName, '{{ nonExistentFunction() }}');
+      let env = new AsyncEnvironment(loader);
+      try {
+        await env.renderTemplate(mainTemplateName, {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain(`(${childTemplateName})`);
+      }
+    });
+
+    it('should report correct path for error in parent template', async () => {
+      var parentTemplateName = 'error-extends-parent.njk';
+      var childTemplateName = 'error-extends-child-parent-error.njk';
+      loader.addTemplate(parentTemplateName, '{% block content %}{% endblock %} {{ nonExistentFunction() }}');
+      loader.addTemplate(childTemplateName, '{% extends "' + parentTemplateName + '" %}{% block content %}Hello{% endblock %}');
+      let env = new AsyncEnvironment(loader);
+      try {
+        await env.renderTemplate(childTemplateName, {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain(`(${parentTemplateName})`);
+      }
+    });
+
+    it('should report correct path for error in child template', async () => {
+      var parentTemplateName = 'error-extends-parent-child-error.njk';
+      var childTemplateName = 'error-extends-child.njk';
+      loader.addTemplate(parentTemplateName, '{% block content %}{% endblock %}');
+      loader.addTemplate(childTemplateName, '{% extends "' + parentTemplateName + '" %}{% block content %}{{ nonExistentFunction() }}{% endblock %}');
+      let env = new AsyncEnvironment(loader);
+      try {
+        await env.renderTemplate(childTemplateName, {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain(`(${childTemplateName})`);
+      }
+    });
+
+    it('should report correct path for imported macro', async () => {
+      var mainTemplateName = 'error-macro-main.njk';
+      var macroTemplateName = 'error-macro-lib.njk';
+      loader.addTemplate(mainTemplateName, '{% import "' + macroTemplateName + '" as lib %}{{ lib.myMacro() }}');
+      loader.addTemplate(macroTemplateName, '{% macro myMacro() %}{{ nonExistentFunction() }}{% endmacro %}');
+      let env = new AsyncEnvironment(loader);
+      try {
+        await env.renderTemplate(mainTemplateName, {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain(`(${macroTemplateName})`);
+      }
+    });
+
+    it('should report correct path for nested include', async () => {
+      var mainTemplateName = 'error-nested-include-main.njk';
+      var templateA = 'error-nested-include-a.njk';
+      var templateB = 'error-nested-include-b.njk';
+      loader.addTemplate(mainTemplateName, '{% include "' + templateA + '" %}');
+      loader.addTemplate(templateA, '{% include "' + templateB + '" %}');
+      loader.addTemplate(templateB, '{{ nonExistentFunction() }}');
+      let env = new AsyncEnvironment(loader);
+      try {
+        await env.renderTemplate(mainTemplateName, {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain(`(${templateB})`);
+      }
+    });
+
+    it('should report correct path for throwOnUndefined error', async () => {
+      var templateName = 'error-throw-on-undefined.njk';
+      loader.addTemplate(templateName, '{{ undefinedVar }}');
+      let env = new AsyncEnvironment(loader, {
+        throwOnUndefined: true
+      });
+      try {
+        await env.renderTemplate(templateName, {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain(`(${templateName})`);
+      }
+    });
+
+    it('should report (unknown path) for string templates', async () => {
+      let env = new AsyncEnvironment(loader);
+      try {
+        await env.renderTemplateString('{{ nonExistentFunction() }}', {});
+        expect().fail('Expected an error to be thrown');
+      } catch (err) {
+        expect(err.message).to.contain('(unknown path)');
+      }
+    });
+  });
+})();
